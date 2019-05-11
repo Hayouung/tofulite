1bcef7eac8018535ff124a2bfba6e0767e606a71
hayouung <hayouung@gmail.com>
2019-05-11 01:32:53 +0100

    exclude test from compilation, selectquery order by test

--- test/SelectQuery.test.ts
+++ test/SelectQuery.test.ts
@@ -8,29 +8,45 @@ describe("#SelectQuery", () => {
 			sq = new SelectQuery("TABLE_NAME");
 		});
 
-		it("should get sql for select statement with * by default", () => {
-			expect(sq.getSql()).toBe("SELECT * FROM TABLE_NAME");
+		describe("basic select query", () => {
+			it("should get sql for select statement with * by default", () => {
+				expect(sq.getSql()).toBe("SELECT * FROM TABLE_NAME");
+			});
+
+			it("should get sql for select statement with a column", () => {
+				sq.selectColumn("COLUMN_ONE");
+				expect(sq.getSql()).toBe("SELECT COLUMN_ONE FROM TABLE_NAME");
+			});
+
+			it("should get sql for select statement with multiple columns", () => {
+				sq.selectColumns(["COLUMN_ONE", "COLUMN_TWO", "COLUMN_THREE"]);
+				expect(sq.getSql()).toBe("SELECT COLUMN_ONE, COLUMN_TWO, COLUMN_THREE FROM TABLE_NAME");
+			});
 		});
 
-		it("should get sql for select statement with a column", () => {
-			sq.selectColumn("COLUMN_ONE");
-			expect(sq.getSql()).toBe("SELECT COLUMN_ONE FROM TABLE_NAME");
+		describe("select query with where", () => {
+			it("should get sql with where conditions and parameter values and put ANDs and ORs correctly", () => {
+				sq.addWheres([
+					{ columnName: "COLUMN_ONE", value: [1, 2, 3], type: "AND" },
+					{ columnName: "COLUMN_TWO",  value: 4, type: "AND" },
+					{ columnName: "COLUMN_THREE", value: [5, 6], type: "OR" },
+					{ columnName: "COLUMN_FOUR", value: 7, type: "AND", operator: ">" }
+				]);
+				expect(sq.getSql()).toBe("SELECT * FROM TABLE_NAME WHERE COLUMN_ONE IN (?, ?, ?) AND COLUMN_TWO = ? AND COLUMN_FOUR > ? OR COLUMN_THREE IN (?, ?)");
+				expect(sq.getValues()).toEqual([1, 2, 3, 4, 7, 5, 6]);
+			});
 		});
 
-		it("should get sql for select statement with multiple columns", () => {
-			sq.selectColumns(["COLUMN_ONE", "COLUMN_TWO", "COLUMN_THREE"]);
-			expect(sq.getSql()).toBe("SELECT COLUMN_ONE, COLUMN_TWO, COLUMN_THREE FROM TABLE_NAME");
-		});
+		describe("select query with order by", () => {
+			it("should order by column ascending if not specified", () => {
+				sq.setOrder("COLUMN_ONE");
+				expect(sq.getSql()).toBe("SELECT * FROM TABLE_NAME ORDER BY COLUMN_ONE ASC");
+			});
 
-		it("should get sql with where conditions and parameter values and put ANDs and ORs correctly", () => {
-			sq.addWheres([
-				{ columnName: "COLUMN_ONE", value: [1, 2, 3], type: "AND" },
-				{ columnName: "COLUMN_TWO",  value: 4, type: "AND" },
-				{ columnName: "COLUMN_THREE", value: [5, 6], type: "OR" },
-				{ columnName: "COLUMN_FOUR", value: 7, type: "AND", operator: ">" }
-			]);
-			expect(sq.getSql()).toBe("SELECT * FROM TABLE_NAME WHERE COLUMN_ONE IN (?, ?, ?) AND COLUMN_TWO = ? AND COLUMN_FOUR > ? OR COLUMN_THREE IN (?, ?)");
-			expect(sq.getValues()).toEqual([1, 2, 3, 4, 7, 5, 6]);
+			it("should order by column descending", () => {
+				sq.setOrder("COLUMN_ONE", true);
+				expect(sq.getSql()).toBe("SELECT * FROM TABLE_NAME ORDER BY COLUMN_ONE DESC");
+			});
 		});
 	});
 
