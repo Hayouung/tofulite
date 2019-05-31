663b8de923e493acd32e60aecfc92c0936ea0d4d
hayouung <hayouung@gmail.com>
2019-05-31 22:14:45 +0100

    add insert query test

---
+++ tests/InsertQuery.test.ts
@@ -0,0 +1,18 @@
+import { InsertQuery } from "../src/lib/queries/InsertQuery";
+
+describe("#DeleteQuery", () => {
+	let iq: InsertQuery;
+
+	beforeEach(() => {
+		iq = new InsertQuery("TABLE_NAME");
+		iq.setValues([
+			{ columnName: "COLUMN_ONE", value: "TEXT" },
+			{ columnName: "COLUMN_TWO", value: 5 }
+		]);
+	});
+
+	it("should get sql for insert query", () => {
+		expect(iq.getSql()).toBe("INSERT INTO TABLE_NAME (COLUMN_ONE, COLUMN_TWO) VALUES (?, ?)");
+		expect(iq.getValues()).toEqual(["TEXT", 5]);
+	});
+});
